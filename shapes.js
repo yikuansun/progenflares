d9e684a28c66a99493c9ceafc189cbc27562ff29
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-01 22:16:26 -0400

    Maybe not use SVG???

--- shapes.js
+++ shapes.js
@@ -6,13 +6,14 @@ function regpoly(x, y, s, r, theta) {
         points.push([x + Math.cos(myvector[1]) * myvector[0], y + Math.sin(myvector[1]) * myvector[0]]);
         myvector[1] += 2 * Math.PI / s;
     }
-    var outstring = "";
+    /*var outstring = "";
     for (var point of points) {
         outstring += point[0].toString() + "," + point[1].toString() + " ";
     }
     var element = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
     element.setAttribute("points", outstring);
-    return element;
+    return element;*/
+    return points;
 }
 
 function star(x, y, s, r1, r2, theta) {
@@ -27,11 +28,12 @@ function star(x, y, s, r1, r2, theta) {
         points.push([x + Math.cos(myvector[1]) * myvector[0], y + Math.sin(myvector[1]) * myvector[0]]);
         myvector[1] += 2 * Math.PI / (s * 2);
     }
-    var outstring = "";
+    /*var outstring = "";
     for (var point of points) {
         outstring += point[0].toString() + "," + point[1].toString() + " ";
     }
     var element = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
     element.setAttribute("points", outstring);
-    return element;
+    return element;*/
+    return points;
 }
