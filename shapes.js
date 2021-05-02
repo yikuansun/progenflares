471efaa7719addafa24b91958ac68117f82b06d6
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-01 21:36:03 -0400

    add star abilities

--- shapes.js
+++ shapes.js
@@ -14,3 +14,24 @@ function regpoly(x, y, s, r, theta) {
     element.setAttribute("points", outstring);
     return element;
 }
+
+function star(x, y, s, r1, r2, theta) {
+    // center x, center y, sides, inner radius, outer radius, rotation
+    var points = [];
+    var myvector = [r2, theta];
+    for (var i = 0; i < s; i++) {
+        myvector[0] = r2;
+        points.push([x + Math.cos(myvector[1]) * myvector[0], y + Math.sin(myvector[1]) * myvector[0]]);
+        myvector[1] += 2 * Math.PI / (s * 2);
+        myvector[0] = r1;
+        points.push([x + Math.cos(myvector[1]) * myvector[0], y + Math.sin(myvector[1]) * myvector[0]]);
+        myvector[1] += 2 * Math.PI / (s * 2);
+    }
+    var outstring = "";
+    for (var point of points) {
+        outstring += point[0].toString() + "," + point[1].toString() + " ";
+    }
+    var element = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    element.setAttribute("points", outstring);
+    return element;
+}
