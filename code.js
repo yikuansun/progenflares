dde56bf8c053853f791b95f1eda30e67dc8287e9
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-03 15:59:26 -0400

    Update code.js

--- code.js
+++ code.js
@@ -66,25 +66,25 @@ function test_svg_download() {
     a.click();
 }
 
-function pngify(svgElem) {
+async function pngify(svgElem) {
     var svgData = new XMLSerializer().serializeToString(svgElem);
     var imgElem = document.createElement("img");
     imgElem.src = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData)));
-    imgElem.onload = function() {
-        var svgClientRect = {
-            width: parseFloat(svgElem.getAttribute("viewBox").split(" ")[2]),
-            height: parseFloat(svgElem.getAttribute("viewBox").split(" ")[3])
-        };
-        var canvas = document.createElement("canvas");
-        canvas.width = svgClientRect.width;
-        canvas.height = svgClientRect.height;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(imgElem, 0, 0, svgClientRect.width, svgClientRect.height);
-        var downloadLink = document.createElement("a");
-        downloadLink.href = canvas.toDataURL("image/png");
-        downloadLink.download = "my_lens_flare.png";
-        downloadLink.click();
-    }
+    const myPromise = new Promise((resolve, reject) => {
+        imgElem.onload = function() {
+            var svgClientRect = {
+                width: parseFloat(svgElem.getAttribute("viewBox").split(" ")[2]),
+                height: parseFloat(svgElem.getAttribute("viewBox").split(" ")[3])
+            };
+            var canvas = document.createElement("canvas");
+            canvas.width = svgClientRect.width;
+            canvas.height = svgClientRect.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(imgElem, 0, 0, svgClientRect.width, svgClientRect.height);
+            resolve(canvas.toDataURL("image/png"));
+        }
+    });
+    return await myPromise;
 }
 
 drawFlare(view, {
