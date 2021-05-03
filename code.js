12aac799fb077d7f6af8bbe278da2898f3df9d6a
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-02 23:35:24 -0400

    y

--- code.js
+++ code.js
@@ -13,3 +13,23 @@ bgRect.style.fill = "black";
 svg.appendChild(bgRect);
 
 var view = document.createElementNS(namespace, "g");
+svg.appendChild(view);
+
+function drawFlare(view, flareconfig) {
+    view.innerHTML = "";
+    var starburst = star(flareconfig.lightx, flareconfig.lighty, flareconfig.star1.sides, flareconfig.star1.innerRadius, flareconfig.star1.outerRadius, 0);
+    view.appendChild(starburst);
+}
+
+drawFlare(view, {
+    lightx: 100,
+    lighty: 100,
+    toX: 400,
+    toY: 225,
+    star1: {
+        sides: 12,
+        innerRadius: 8,
+        outerRadius: 69,
+        rotation: 0
+    }
+});
