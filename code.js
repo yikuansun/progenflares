4ed13bbf5dbf07de38dabfb622a3fe01924aef6c
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-02 23:28:31 -0400

    Add some files

---
+++ code.js
@@ -0,0 +1,13 @@
+var searchInfo = new URLSearchParams(location.search);
+var docWidth = parseFloat(searchInfo.get("docWidth"));
+var docHeight = parseFloat(searchInfo.get("docHeight"));
+
+var svg = document.querySelector("svg");
+var namespace = "http://www.w3.org/2000/svg";
+svg.setAttribute("viewBox", `0 0 ${docWidth} ${docHeight}`);
+
+var bgRect = document.createElementNS(namespace, "rect");
+bgRect.setAttribute("x", 0); bgRect.setAttribute("y", 0);
+bgRect.setAttribute("width", docWidth); bgRect.setAttribute("height", docHeight);
+bgRect.style.fill = "black";
+svg.appendChild(bgRect);
