5e2caa1bfb7945dfff84cb778de20d12e5bdefee
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-04 15:27:24 -0400

    ee

--- code.js
+++ code.js
@@ -62,6 +62,7 @@ function drawFlare(view, flareconfig) {
     colorLayer1.style.mixBlendMode = "overlay";
     view.appendChild(colorLayer1);
     var multiIris = document.createElementNS(namespace, "g");
+    multiIris.setAttribute("opacity", flareconfig.multiIris.opacity);
     view.appendChild(multiIris);
     var currentx = 0;
     var currenty = 0;
@@ -71,7 +72,7 @@ function drawFlare(view, flareconfig) {
         if (Math.random() < 0.35) {
             var sclFac = Math.random() * Math.abs(i) / 25;
             var iris = regpoly(currentx, currenty, flareconfig.multiIris.sides, flareconfig.multiIris.radius * sclFac, flareconfig.multiIris.rotation);
-            iris.setAttribute("fill-opacity", flareconfig.multiIris.opacity / sclFac);
+            iris.setAttribute("fill-opacity", flareconfig.multiIris.innerOpacity / sclFac);
             iris.style.fill = flareconfig.multiIris.color;
             iris.style.mixBlendMode = "screen";
             multiIris.setAttribute("filter", "url(#MIBlur)");
@@ -162,7 +163,8 @@ drawFlare(view, {
         sides: 6,
         radius: 69,
         rotation: 0,
-        opacity: 0.1,
+        opacity: 1,
+        innerOpacity: 0.1,
         blur: 2
     },
 });
