f84255cd62dd0b1f2fccd3f4c4c6c9b3268e2e75
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-05 19:06:54 -0400

    Portal
    prob not gonna use this

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 var searchInfo = new URLSearchParams(location.search);
 var docWidth = parseFloat(searchInfo.get("docWidth"));
 var docHeight = parseFloat(searchInfo.get("docHeight"));
+var portal = searchInfo.get("portal");
 
 var svg = document.querySelector("svg");
 var namespace = "http://www.w3.org/2000/svg";
