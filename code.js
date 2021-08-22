02825ae748fe6d594ecaaa5b14ae0fec33f5581d
yikuansun <yikuansun2015@gmail.com>
2021-08-22 15:48:40 -0400

    Use colors

--- code.js
+++ code.js
@@ -208,7 +208,7 @@ document.querySelector("#randomizeMI").addEventListener("click", function() { Ra
 document.querySelector("#randomizedirt").addEventListener("click", function() { Randomizer(document.querySelector("#orbs_seed")); });
 
 var tabs = document.querySelectorAll("#tabbar td");
-tabs[0].style.backgroundColor = "white";
+tabs[0].style.backgroundColor = "var(--ui-tertiary-color)";
 var panelsections = document.querySelectorAll("#controlpanel div:not(#emptypanelsection)");
 panelsections[0].style.display = "block";
 
@@ -218,7 +218,7 @@ for (var i = 0; i < tabs.length; i++) {
             if (j == ${i}) {
                 if (panelsections[j].style.display != "block") {
                     panelsections[j].style.display = "block";
-                    tabs[j].style.backgroundColor = "white";
+                    tabs[j].style.backgroundColor = "var(--ui-tertiary-color)";
 
                     document.querySelector("#emptypanelsection").style.display = "none";
                 }
