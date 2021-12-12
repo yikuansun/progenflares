3b990d78830fc98f7292f54ba61e5783973c4754
yikuansun <yikuansun2015@gmail.com>
2021-12-12 09:36:49 -0500

    Update helpbutton.js

--- helpbutton.js
+++ helpbutton.js
@@ -7,9 +7,9 @@ helpbutton.style.bottom = "10px";
 helpbutton.style.right = "10px";
 helpbutton.style.backgroundImage = "url('images/help_icon.svg')";
 helpbutton.style.backgroundSize = "100% 100%";
-helpbutton.style.opacity = "0.69";
+helpbutton.style.opacity = "0.5";
 helpbutton.addEventListener("mouseover", function(e) { this.style.opacity = "0.85" });
-helpbutton.addEventListener("mouseout", function(e) { this.style.opacity = "0.69" });
+helpbutton.addEventListener("mouseout", function(e) { this.style.opacity = "0.5" });
 helpbutton.addEventListener("click", function(e) {
     var a = document.createElement("a");
     a.target = "_blank";
